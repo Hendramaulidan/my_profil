35a5f289219c020bfb7c2a078dda06004df9cbec
hendramaulidan <hendra19ma@gmail.com>
2020-01-25 10:25:55 +0700

    aku hendra maulidan

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -5,10 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="{{url('assets/css/bootstrap.min.css')}}">
-    <link rel="stylesheet" href="{{url('assets/fontawesome/css/fontawesome.css')}}">
-    
+    <link rel="stylesheet" href="{{url('assets/fontawesome/css/fontawesome.min.css')}}">    
     <link href="{{url('assets/img/hendra.jpg')}}" rel="shortcut icon"/>
-
     <title>Profilku</title>
     <style>
     .bulat{
